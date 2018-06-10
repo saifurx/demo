e95eb0749361cb55eeecbd2854083658eefa5bae
Administrator <Administrator@R0349>
2018-06-10 16:59:00 +0530

    new field added

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
+import java.util.Optional;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,32 +11,52 @@ import javax.persistence.Id;
 @Entity
 public class Book {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    int id;
-    String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	@Column
+	String bookName;
+	@Column
+	String author;
 
-    public int getId() {
-        return id;
-    }
+	public Book() {
+		super();
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public Book(int id, String bookName, String author) {
+		super();
+		this.id = id;
+		this.bookName = bookName;
+		this.author = author;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", bookName=" + bookName + ", author=" + author + "]";
+	}
+
 }
